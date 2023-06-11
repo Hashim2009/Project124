42bb6da514c7906da3428ee31e72cadcd9a96919
Hashim2009 <94174587+Hashim2009@users.noreply.github.com>
2023-06-11 13:30:53 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+
+    canvas = createCanvas(400, 400);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('Posenet Is initalized');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+    }
+}
